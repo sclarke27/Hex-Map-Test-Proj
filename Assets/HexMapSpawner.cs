e451da1d0c6c0fe21bf69911bf1abe078afc68da
Scott Clarke <sclarke27@gmail.com>
2015-10-23 18:51:13 -0700

    first add

---
+++ Assets/HexMapSpawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexMapSpawner : MonoBehaviour {
+
+    private HexMapTile baseTileObject;
+    private bool mapDirty = true;
+    private int totalTilesX = 8;
+    private int totalTilesZ = 6;
+    private int tileWidth = 2;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Start Hex Map");
+        baseTileObject = GameObject.FindObjectOfType<HexMapTile>();
+        
+
+        
+	}
+
+    private void DrawMapTiles()
+    {
+        float startX = ((tileWidth * totalTilesX) / 2) * -1;
+        float startZ = ((tileWidth * totalTilesZ) / 2) * -1;
+
+        baseTileObject.gameObject.SetActive(true);
+        for (var x = 0; x < totalTilesX; x++)
+        {
+            for (var z = 0; z < totalTilesZ; z++)
+            {
+                float currX = startX + (tileWidth * x);
+                float currZ = startZ + (tileWidth * z);
+                Instantiate(baseTileObject, new Vector3(currX, 0, currZ), Quaternion.identity);
+            }
+
+        }
+            
+        baseTileObject.gameObject.SetActive(false);
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (mapDirty)
+        {
+            mapDirty = false;
+            Debug.Log("Update Hex Map");
+            DrawMapTiles();
+            
+        }
+	}
+}
---
+++ Assets/HexMapTile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexMapTile : MonoBehaviour {
+
+    private MeshFilter tileMesh;
+    private float tileWidth = 0;
+    private float tileHeight = 0;
+
+    public float TileWidth
+    {
+        get { return tileWidth; }
+        set { tileWidth = value; }
+    }
+
+    public float TileHeight
+    {
+        get { return tileHeight; }
+        set { tileHeight = value; }
+    }
+    
+    // Use this for initialization
+	void Start () {
+        tileMesh = transform.GetComponent("Tile_Mesh") as MeshFilter; ;//transform.GetComponent<MeshFilter>("Tile_Mesh");
+        tileWidth = tileMesh.mesh.bounds.extents.x * 2;
+        tileHeight = tileMesh.mesh.bounds.extents.y * 2;
+	}
+
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(tileMesh);
+	}
+}
