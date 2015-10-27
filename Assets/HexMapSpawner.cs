94a58d1fc0bd2dc3e4a205d85de16770eac3e281
Scott Clarke <sclarke27@gmail.com>
2015-10-26 21:41:10 -0700

    hex map updates

--- Assets/HexMapSpawner.cs
+++ Assets/HexMapSpawner.cs
@@ -5,9 +5,11 @@ public class HexMapSpawner : MonoBehaviour {
 
     private HexMapTile baseTileObject;
     private bool mapDirty = true;
-    private int totalTilesX = 8;
-    private int totalTilesZ = 6;
-    private int tileWidth = 2;
+    private int totalTilesX = 14;
+    private int totalTilesZ = 28;
+    private float tileWidth = 2.0f;
+
+	public Camera mainCamera;
 
 	// Use this for initialization
 	void Start () {
@@ -20,17 +22,21 @@ public class HexMapSpawner : MonoBehaviour {
 
     private void DrawMapTiles()
     {
-        float startX = ((tileWidth * totalTilesX) / 2) * -1;
-        float startZ = ((tileWidth * totalTilesZ) / 2) * -1;
+		float startX = ((tileWidth * totalTilesX) / 2) * -1;
+		float startZ = ((tileWidth * totalTilesZ) / 2) * -1;
 
         baseTileObject.gameObject.SetActive(true);
         for (var x = 0; x < totalTilesX; x++)
         {
             for (var z = 0; z < totalTilesZ; z++)
             {
-                float currX = startX + (tileWidth * x);
-                float currZ = startZ + (tileWidth * z);
-                Instantiate(baseTileObject, new Vector3(currX, 0, currZ), Quaternion.identity);
+				float currX = startX + ((tileWidth * (tileWidth * 0.75f)) * x) + ((z%2!=1) ? (tileWidth*0.75f) : 0);
+				float currZ = startZ + ((tileWidth / 2.3f) * z);
+
+                Instantiate(baseTileObject, new Vector3(-currX, 0, -currZ), Quaternion.identity);
+
+
+
             }
 
         }
@@ -44,9 +50,16 @@ public class HexMapSpawner : MonoBehaviour {
         if (mapDirty)
         {
             mapDirty = false;
-            Debug.Log("Update Hex Map");
             DrawMapTiles();
             
         }
+		if (Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.D)) {
+			if(Input.GetKeyDown(KeyCode.A)) {
+				mainCamera.transform.localPosition = new Vector3((mainCamera.transform.localPosition.x-0.2f), mainCamera.transform.localPosition.y, mainCamera.transform.localPosition.z);
+			} else {
+				mainCamera.transform.localPosition = new Vector3((mainCamera.transform.localPosition.x+0.2f), mainCamera.transform.localPosition.y, mainCamera.transform.localPosition.z);
+
+			}
+		}
 	}
 }
--- Assets/HexMapTile.cs
+++ Assets/HexMapTile.cs
@@ -1,33 +1,71 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class HexMapTile : MonoBehaviour {
 
     private MeshFilter tileMesh;
-    private float tileWidth = 0;
-    private float tileHeight = 0;
+    private float _tileWidth = 0;
+    private float _tileHeight = 0;
+	private bool _isSelected = false;
+	private Color defaultColor;
+	public Text coordText;
 
     public float TileWidth
     {
-        get { return tileWidth; }
-        set { tileWidth = value; }
+        get { return _tileWidth; }
+        set { _tileWidth = value; }
     }
 
     public float TileHeight
     {
-        get { return tileHeight; }
-        set { tileHeight = value; }
+        get { return _tileHeight; }
+        set { _tileHeight = value; }
     }
+
+	public bool IsSelected 
+	{
+		get { return _isSelected; }
+		set { _isSelected = value; }
+	}
     
     // Use this for initialization
 	void Start () {
-        tileMesh = transform.GetComponent("Tile_Mesh") as MeshFilter; ;//transform.GetComponent<MeshFilter>("Tile_Mesh");
-        tileWidth = tileMesh.mesh.bounds.extents.x * 2;
-        tileHeight = tileMesh.mesh.bounds.extents.y * 2;
+		tileMesh = transform.GetComponentInChildren<MeshFilter> ();
+		if (tileMesh == null) {
+			Debug.LogError ("tile mesh not found", tileMesh);
+			return;
+		}
+		float randomHeight = Random.Range (0.1f, 1f);
+		TileWidth = tileMesh.mesh.bounds.extents.x * 2;
+		TileHeight = tileMesh.mesh.bounds.extents.y * 2;
+		defaultColor = new Color ();
+		defaultColor.r = randomHeight;
+		defaultColor.g = randomHeight;
+		defaultColor.b = 1f;
+		tileMesh.renderer.materials[0].color = defaultColor;
+		tileMesh.transform.localScale = new Vector3 (tileMesh.transform.localScale.x, randomHeight, tileMesh.transform.localScale.z);
+		coordText.text = tileMesh.transform.localScale.x + "," + tileMesh.transform.localScale.y;
 	}
 
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(tileMesh);
+        //Debug.Log(tileMesh);
+	}
+
+	void OnMouseDown() {
+		Debug.Log ("click");
+		IsSelected = !IsSelected;
+		Color tempColor = new Color ();
+		if (IsSelected) {
+			tempColor.r = 1f;
+			tempColor.g = 0f;
+			tempColor.b = 0f;
+		} else {
+			tempColor = defaultColor;
+		}
+		tileMesh.renderer.materials[0].color = tempColor;
+
+
 	}
 }
