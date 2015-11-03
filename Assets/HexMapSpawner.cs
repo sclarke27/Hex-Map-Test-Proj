64e840d3ef35c7e35c1c8ac8da9976f21031c870
Scott Clarke <sclarke27@gmail.com>
2015-11-02 18:21:12 -0800

    moar updates

--- Assets/HexMapSpawner.cs
+++ Assets/HexMapSpawner.cs
@@ -5,9 +5,10 @@ public class HexMapSpawner : MonoBehaviour {
 
     private HexMapTile baseTileObject;
     private bool mapDirty = true;
-    private int totalTilesX = 14;
-    private int totalTilesZ = 28;
+    private int totalTilesX = 5;
+    private int totalTilesZ = 15;
     private float tileWidth = 2.0f;
+	private bool isKeyDown = false;
 
 	public Camera mainCamera;
 
@@ -15,27 +16,25 @@ public class HexMapSpawner : MonoBehaviour {
 	void Start () {
         Debug.Log("Start Hex Map");
         baseTileObject = GameObject.FindObjectOfType<HexMapTile>();
-        
-
-        
 	}
 
     private void DrawMapTiles()
     {
-		float startX = ((tileWidth * totalTilesX) / 2) * -1;
-		float startZ = ((tileWidth * totalTilesZ) / 2) * -1;
+		float startX = 0;//((tileWidth * totalTilesX) / 2) * -1;
+		float startZ = 0;//((tileWidth * totalTilesZ) / 2) * -1;
 
         baseTileObject.gameObject.SetActive(true);
-        for (var x = 0; x < totalTilesX; x++)
+		for (var x = -totalTilesX; x < totalTilesX; x++)
         {
-            for (var z = 0; z < totalTilesZ; z++)
+			for (var z = -totalTilesZ; z < totalTilesZ; z++)
             {
-				float currX = startX + ((tileWidth * (tileWidth * 0.75f)) * x) + ((z%2!=1) ? (tileWidth*0.75f) : 0);
-				float currZ = startZ + ((tileWidth / 2.3f) * z);
-
-                Instantiate(baseTileObject, new Vector3(-currX, 0, -currZ), Quaternion.identity);
-
-
+				int rowNum = (z < 0) ? -z : z;
+				Debug.Log(rowNum);
+				//float currX = startX + ((tileWidth * (tileWidth * 0.75f)) * x) + ((rowNum%2!=1) ? (tileWidth*0.75f) : 0);
+				//float currZ = startZ + ((tileWidth / 2.3f) * z);
+				HexMapTile tempTile = Instantiate(baseTileObject, new Vector3(0, 0, 0), Quaternion.identity) as HexMapTile;
+				tempTile.SetTileCoords(new Vector3(x, 0f, z));
+				//tempTile.SetTilePos(new Vector3(-currX, 0, -currZ));
 
             }
 
@@ -53,13 +52,41 @@ public class HexMapSpawner : MonoBehaviour {
             DrawMapTiles();
             
         }
-		if (Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.D)) {
-			if(Input.GetKeyDown(KeyCode.A)) {
+
+		if(Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.S) || Input.GetKeyDown (KeyCode.D)
+		    || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.DownArrow) || Input.GetKeyDown (KeyCode.LeftArrow) 
+		    || Input.GetKeyDown (KeyCode.RightArrow) ) {
+
+			isKeyDown = true;
+		}
+
+		if(Input.GetKeyUp (KeyCode.W) || Input.GetKeyUp (KeyCode.A) || Input.GetKeyUp (KeyCode.S) || Input.GetKeyUp (KeyCode.D)
+		    || Input.GetKeyUp (KeyCode.UpArrow) || Input.GetKeyUp (KeyCode.DownArrow) || Input.GetKeyUp (KeyCode.LeftArrow) 
+		    || Input.GetKeyUp (KeyCode.RightArrow) ) 
+		{
+			
+			isKeyDown = false;
+		}
+
+
+		if (isKeyDown) {
+
+			if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
 				mainCamera.transform.localPosition = new Vector3((mainCamera.transform.localPosition.x-0.2f), mainCamera.transform.localPosition.y, mainCamera.transform.localPosition.z);
-			} else {
+			} 
+			if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
 				mainCamera.transform.localPosition = new Vector3((mainCamera.transform.localPosition.x+0.2f), mainCamera.transform.localPosition.y, mainCamera.transform.localPosition.z);
 
 			}
+
+			if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+				mainCamera.transform.localPosition = new Vector3((mainCamera.transform.localPosition.x), mainCamera.transform.localPosition.y, mainCamera.transform.localPosition.z+0.2f);
+			} 
+			if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+				mainCamera.transform.localPosition = new Vector3((mainCamera.transform.localPosition.x), mainCamera.transform.localPosition.y, mainCamera.transform.localPosition.z-0.2f);
+				
+			}
 		}
+
 	}
 }
--- Assets/HexMapTile.cs
+++ Assets/HexMapTile.cs
@@ -5,11 +5,13 @@ using System.Collections;
 public class HexMapTile : MonoBehaviour {
 
     private MeshFilter tileMesh;
-    private float _tileWidth = 0;
-    private float _tileHeight = 0;
+    private float _tileWidth = 2;
+    private float _tileHeight = 2;
 	private bool _isSelected = false;
+	private Vector3 _tileCoords = new Vector2();
 	private Color defaultColor;
 	public Text coordText;
+	public Text posText;
 
     public float TileWidth
     {
@@ -29,6 +31,12 @@ public class HexMapTile : MonoBehaviour {
 		set { _isSelected = value; }
 	}
     
+	public Vector3 TileCoords 
+	{
+		get { return _tileCoords; }
+		set { _tileCoords = value; }
+	}
+
     // Use this for initialization
 	void Start () {
 		tileMesh = transform.GetComponentInChildren<MeshFilter> ();
@@ -37,20 +45,32 @@ public class HexMapTile : MonoBehaviour {
 			return;
 		}
 		float randomHeight = Random.Range (0.1f, 1f);
-		TileWidth = tileMesh.mesh.bounds.extents.x * 2;
-		TileHeight = tileMesh.mesh.bounds.extents.y * 2;
 		defaultColor = new Color ();
 		defaultColor.r = randomHeight;
 		defaultColor.g = randomHeight;
 		defaultColor.b = 1f;
 		tileMesh.renderer.materials[0].color = defaultColor;
 		tileMesh.transform.localScale = new Vector3 (tileMesh.transform.localScale.x, randomHeight, tileMesh.transform.localScale.z);
-		coordText.text = tileMesh.transform.localScale.x + "," + tileMesh.transform.localScale.y;
+
 	}
 
 	// Update is called once per frame
 	void Update () {
         //Debug.Log(tileMesh);
+	}
+
+	public void SetTilePos(Vector3 pos) {
+		transform.position = pos;
+		posText.text = Mathf.Round(pos.x) + "," + Mathf.Round(pos.z);
+	}
+
+	public void SetTileCoords(Vector3 coords) {
+		TileCoords = coords;
+		float rowNum = (TileCoords.z < 0) ? -TileCoords.z : TileCoords.z;
+		float currX = ((TileWidth * (TileWidth * 0.75f)) * TileCoords.x) + ((rowNum%2!=1) ? (TileWidth*0.75f) : 0);
+		float currZ = ((TileWidth / 2.3f) * TileCoords.z);
+		SetTilePos (new Vector3 (currX, transform.position.y, currZ));
+		coordText.text = TileCoords.x + "," + TileCoords.z;
 	}
 
 	void OnMouseDown() {
